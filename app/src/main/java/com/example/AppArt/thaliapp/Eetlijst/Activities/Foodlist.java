5cb70f2ba1f6af23590fdff7b2adcdc5f4c3e65a
srnapls <ssmrietbergen@msn.com>
2015-07-07 15:19:08 +0200

    doei

---
+++ app/src/main/java/com/example/AppArt/thaliapp/Eetlijst/Activities/Foodlist.java
@@ -0,0 +1,84 @@
+package com.example.AppArt.thaliapp.Eetlijst.Activities;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.AppArt.thaliapp.R;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+/**
+ * @author Frank Gerlings (s4384873), Lisa Kalse (s4338340), Serena Rietbergen
+ *         (s4182804)
+ */
+
+public class FoodList extends ListActivity {
+
+    private String[] list;
+    private ArrayList<String> chosen = new ArrayList<>();
+    private double amount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foodlist);
+        Bundle extras = getIntent().getExtras();
+        if (extras == null) {
+            list = null;
+        } else {
+            list = extras.getStringArray("foodlist");
+            String[] temp = extras.getStringArray("chosen");
+            if (temp != null) {
+                Collections.addAll(chosen, temp);
+            }
+            amount = extras.getDouble("amount", 0.0);
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, list);
+        setListAdapter(adapter);
+
+        chosen = new ArrayList<>();
+        amount = 0;
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, @NonNull KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            String[] all = new String[chosen.size()];
+            for (int i = 0; i < chosen.size(); i++) {
+                all[i] = chosen.get(i);
+            }
+            Intent i = new Intent(this, Restaurant.class);
+            i.putExtra("chosenfood", all);
+            i.putExtra("chosenamount", amount);
+            startActivity(i);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        String s = l.getItemAtPosition(position).toString();
+        Scanner scan = new Scanner(s);
+        scan.useDelimiter(":");
+        String food = scan.next();
+        chosen.add(food);
+        CharSequence cs = "Je hebt gekozen voor " + food;
+        Toast t = Toast.makeText(this, cs, Toast.LENGTH_SHORT);
+        t.show();
+        String geld = s.substring(s.lastIndexOf("EUR") - 5, s.lastIndexOf("EUR") - 1);
+        double d = Double.parseDouble(geld);
+        amount += d;
+    }
+}
